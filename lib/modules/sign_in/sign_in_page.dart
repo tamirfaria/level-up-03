ae970a73b6433ec449e01c4a70dba66aebafee73
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-04 20:25:25 -0300

    feat: add SignInPage form validator

--- lib/modules/sign_in/sign_in_page.dart
+++ lib/modules/sign_in/sign_in_page.dart
@@ -10,76 +10,98 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 150),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                "Entrar",
-                style: TextStyle(
-                  fontSize: 36,
+        child: Builder(
+          builder: (context) => Center(
+            child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                    const SizedBox(height: 100.0),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 16.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Insira seu e-mail",
+                          labelText: "E-mail",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 5) {
+                            return "Preencha com um e-mail válido";
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 8.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Insira sua senha",
+                          labelText: "Senha",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 5) {
+                            return "Digite uma senha válida";
+                          }
+                        },
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (!_formKey.currentState!.validate()) {
+                              return;
+                            }
+                          },
+                          child: const Text("Entrar"),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 64.0),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 16.0, bottom: 32),
+                      child: Text("Não possui uma conta? | Cadastre-se"),
+                    ),
+                    Center(
+                      child: GoogleButton(onTap: () {}),
+                    ),
+                    const SizedBox(height: 8),
+                    Center(
+                      child: TwitterButton(onTap: () {}),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 100.0),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 16.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Insira seu e-mail",
-                    labelText: "E-mail",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 8.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Insira sua senha",
-                    labelText: "Senha",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Entrar"),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 64.0),
-              const Padding(
-                padding: EdgeInsets.only(top: 16.0, bottom: 32),
-                child: Text("Não possui uma conta? | Cadastre-se"),
-              ),
-              Center(
-                child: GoogleButton(onTap: () {}),
-              ),
-              const SizedBox(height: 8),
-              Center(
-                child: TwitterButton(onTap: () {}),
-              ),
-            ],
+            ),
           ),
         ),
       ),
