cda4b52a43f9459f528c5cd28d2532020f082e78
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-04 20:25:10 -0300

    feat: add ForgotPassWordPage form validator

--- lib/modules/forgot_password/forgot_password_page.dart
+++ lib/modules/forgot_password/forgot_password_page.dart
@@ -8,64 +8,86 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 150),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                "Atualizar senha",
-                style: TextStyle(
-                  fontSize: 32,
+        child: Builder(
+          builder: (context) => Center(
+            child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Atualizar senha",
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(height: 100.0),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 16.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Digite seu e-mail",
+                          labelText: "E-mail",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 5) {
+                            return "Preencha com um e-mail válido";
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 8.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Digite sua nova senha",
+                          labelText: "Nova senha",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 5) {
+                            return "Digite uma senha válida";
+                          }
+                        },
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (!_formKey.currentState!.validate()) {
+                              return;
+                            }
+                          },
+                          child: const Text("Atualizar"),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 100.0),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 16.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Digite seu e-mail",
-                    labelText: "E-mail",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 8.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Digite sua nova senha",
-                    labelText: "Nova senha",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Atualizar"),
-                  ),
-                ],
-              ),
-            ],
+            ),
           ),
         ),
       ),
