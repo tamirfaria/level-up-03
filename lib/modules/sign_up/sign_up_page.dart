8a7270726ced3b2078ca99184e35fd356e7538c0
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-04 20:25:38 -0300

    feat: add SignUpPage form validator

--- lib/modules/sign_up/sign_up_page.dart
+++ lib/modules/sign_up/sign_up_page.dart
@@ -9,6 +9,7 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final maskFormatter = MaskTextInputFormatter(
     mask: '(##) #####-####',
   );
@@ -16,96 +17,127 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 150),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                "Cadastre-se",
-                style: TextStyle(
-                  fontSize: 36,
+        child: Builder(
+          builder: (context) => Center(
+            child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Cadastre-se",
+                      style: TextStyle(
+                        fontSize: 36,
+                      ),
+                    ),
+                    const SizedBox(height: 100.0),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 16.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Escreva seu nome",
+                          labelText: "Nome",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 3) {
+                            return "Preencha com um nome válido";
+                          }
+                        },
+                        keyboardType: TextInputType.name,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 16.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Digite seu telefone",
+                          labelText: "Telefone",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 11) {
+                            return "Preencha com um telefone válido";
+                          }
+                        },
+                        keyboardType: TextInputType.phone,
+                        inputFormatters: [maskFormatter],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 16.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Insira seu e-mail",
+                          labelText: "E-mail",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 5) {
+                            return "Preencha com um e-mail válido";
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24.0,
+                        right: 24.0,
+                        bottom: 8.0,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Insira sua senha",
+                          labelText: "Senha",
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 5) {
+                            return "Digite uma senha válida";
+                          }
+                        },
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (!_formKey.currentState!.validate()) {
+                              return;
+                            }
+                          },
+                          child: const Text("Cadastre-se"),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 64.0),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 16.0, bottom: 32),
+                      child: Text("Já possui uma conta? | Entrar"),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 100.0),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 16.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Escreva seu nome",
-                    labelText: "Nome",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.name,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 16.0,
-                ),
-                child: TextField(
-                  decoration: const InputDecoration(
-                    hintText: "Digite seu telefone",
-                    labelText: "Telefone",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.phone,
-                  inputFormatters: [maskFormatter],
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 16.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Insira seu e-mail",
-                    labelText: "E-mail",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 8.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Insira sua senha",
-                    labelText: "Senha",
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Cadastre-se"),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 64.0),
-              const Padding(
-                padding: EdgeInsets.only(top: 16.0, bottom: 32),
-                child: Text("Já possui uma conta? | Entrar"),
-              ),
-            ],
+            ),
           ),
         ),
       ),
